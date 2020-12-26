52368351bee7dcae3a3eb63c6d2be93490602298
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2020-12-27 00:12:29 +0100

    Update optimization

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,6 @@
 <template>
   <router-view/>
+  <a href="https://www.instagram.com/asiastudeler/" target="_blank" class=" z-50 text-sm text-white font-bold bg-black rounded-tl-xl border border-white bottom-0 right-0 fixed align-bottom px-2 py-1" style="padding-top: 4px !important;">Studeler</a>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style src="./index.css"/>
