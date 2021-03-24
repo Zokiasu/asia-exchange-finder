009dcbaf35152df67eb2bd4e38d0ae3acba086f4
Zokiasu <32719751+Zokiasu@users.noreply.github.com>
2021-03-24 16:48:51 +0100

    Create UniversityEditor.vue

---
+++ src/views/UniversityEditor.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>Hello {{ $route.query.id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+
+    data(){
+      return {
+          
+      }
+    },
+
+    mounted(){
+        console.log(this.$route.query.id)
+    },
+
+    methods:{
+
+    },
+}
+</script>
