034a844ab1b71abf1f76eeb90f73dc295c43ded2
Zokiasu <pierrick.tly@gmail.com>
2022-02-15 12:24:37 +0100

    remove button on card

--- src/components/HomeComponent/UniversityCard.vue
+++ src/components/HomeComponent/UniversityCard.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="Card rounded-lg overflow-hidden h-60 mx-5 ms:mx-auto text-xs md:text-md 4xl:text-lg text-black hover:text-blue-600" @click="openCard(university.universitySourceName, university.universitySourceId)">
         <div class="bg-gray-200 rounded-lg overflow-hidden shadow-2xl relative h-full">
-            <button class="Button bg-blue-500 rounded-3xl m-2 px-2 absolute right-0 block shadow-2xl">More Info</button>
             <p class="absolute w-full bg-white bg-opacity-80 p-5 bottom-0 block">
                 {{ university.universitySourceName }}
             </p>
